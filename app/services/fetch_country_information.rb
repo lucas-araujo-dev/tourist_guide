1de9ecb4342b5ab99da009168213939ef49633bd
lucas araujo <lucas.trabalho.araujo@gmail.com>
2024-04-27 15:42:03 -0300

    feat: ajustado service para buscar informacoes dos pais

--- app/services/fetch_country_information.rb
+++ app/services/fetch_country_information.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class FetchCountryInformation
-  API_URL = 'https://restcountries.com/v3.1'
+  API_URL = 'https://restcountries.com/v3.1/name'
 
   def initialize(country_name)
     @country_name = country_name
   end
 
   def call
-    response = Faraday.get("#{API_URL}/name/#{@country_name}")
+    response = Faraday.get("#{API_URL}/#{@country_name}")
     response.success? ? JSON.parse(response.body).first : nil
   end
 end
